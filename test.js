975a32537bac02e01fb839f2a8bf77c725cdc7ce
baltej223 <73211864+baltej223@users.noreply.github.com>
2022-06-12 10:40:44 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,15 @@
+function search(props) {
+    if (props.firstWordSearch) {
+        for (let i = 0; i <= document.getElementsByClassName('search-true').length; i++) {
+            let ed = document.getElementsByClassName('search-true')[i];
+            ed.innerHTML.slice(0, 1).includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none";
+        }
+    }
+    else if (props.normalSearch) {
+        for (let i = 0; i <= document.getElementsByClassName('search-true').length; i++) {
+            let ed = document.getElementsByClassName('search-true')[i];
+            ed.innerHTML.includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none";
+        }
+    }
+}
+
