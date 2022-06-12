9cb733dcaf23cd50e089a5b50943bd6816153d1a
baltej223 <73211864+baltej223@users.noreply.github.com>
2022-06-12 17:58:50 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,15 +1,34 @@
 function search(props) {
     if (props.firstWordSearch) {
-        for (let i = 0; i <= document.getElementsByClassName('search-true').length; i++) {
-            let ed = document.getElementsByClassName('search-true')[i];
-            ed.innerHTML.slice(0, props.searchData.length).includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none";
+        if (props.hideElement != undefined) {
+            for (let i = 0; i <= document.getElementsByClassName('search-true').length; i++) {
+                let ed = document.getElementsByClassName(props.hideElement)[i];
+                ed.innerHTML.slice(0, props.searchData.length).includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none";
+                props.addClass != undefined ? ed.classList.add(props.addClass) : null;
+            }
+        }
+        else {
+            for (let i = 0; i <= document.getElementsByClassName('search-true').length; i++) {
+                let ed = document.getElementsByClassName('search-true')[i];
+                ed.innerHTML.slice(0, props.searchData.length).includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none";
+                props.addClass != undefined ? ed.classList.add(props.addClass) : null;
+            }
         }
     }
     else if (props.normalSearch) {
-        for (let i = 0; i <= document.getElementsByClassName('search-true').length; i++) {
-            let ed = document.getElementsByClassName('search-true')[i];
-            ed.innerHTML.includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none";
+        if (props.hideElement != undefined) {
+            for (let i = 0; i <= document.getElementsByClassName('search-true').length; i++) {
+                let ed = document.getElementsByClassName(props.hideElement)[i];
+                ed.innerHTML.includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none";
+                props.addClass != undefined ? ed.classList.add(props.addClass) : null;
+            }
+        }
+        else {
+            for (let i = 0; i <= document.getElementsByClassName('search-true').length; i++) {
+                let ed = document.getElementsByClassName('search-true')[i];
+                ed.innerHTML.includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none";
+                props.addClass != undefined ? ed.classList.add(props.addClass) : null;
+            }
         }
     }
 }
-
