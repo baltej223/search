4d3c9cb7796d5576f8de73053cec308e28f60d88
baltej223 <73211864+baltej223@users.noreply.github.com>
2022-06-12 11:07:24 +0530

    Update search.js

--- search.js
+++ search.js
@@ -2,7 +2,7 @@ function search(props) {
     if (props.firstWordSearch) {
         for (let i = 0; i <= document.getElementsByClassName('search-true').length; i++) {
             let ed = document.getElementsByClassName('search-true')[i];
-            ed.innerHTML.slice(0, 1).includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none";
+            ed.innerHTML.slice(0, props.searchData.length).includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none";
         }
     }
     else if (props.normalSearch) {
