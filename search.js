ef325160dcc1f10cdc788fe270505754d44d493d
baltej223 <73211864+baltej223@users.noreply.github.com>
2022-06-14 13:10:11 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,34 +1,67 @@
-function search(props) {
+let search = (props) => {
     if (props.firstWordSearch) {
-        if (props.hideElement != undefined) {
+        if (props.hideAnotherElement != undefined) {
+            props.onSearch != undefined ? setTimeout(props.onSearch, 1) : null;
             for (let i = 0; i <= document.getElementsByClassName('search-true').length; i++) {
-                let ed = document.getElementsByClassName(props.hideElement)[i];
-                ed.innerHTML.slice(0, props.searchData.length).includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none";
-                props.addClass != undefined ? ed.classList.add(props.addClass) : null;
+                let ed = document.getElementsByClassName(props.hideAnotherElement)[i];
+                let md = document.getElementsByClassName('search-true')[i];
+                if (props.hide === false) { null; }
+                else { md.innerHTML.slice(0, props.searchData.length).includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none"; }
+                props.addClass != undefined && md.innerHTML.slice(0, props.searchData.length).includes(props.searchData) ? ed.classList.add(props.addClass) : null;
             }
+
         }
         else {
+            props.onSearch != undefined ? setTimeout(props.onSearch, 1) : null;
             for (let i = 0; i <= document.getElementsByClassName('search-true').length; i++) {
                 let ed = document.getElementsByClassName('search-true')[i];
-                ed.innerHTML.slice(0, props.searchData.length).includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none";
-                props.addClass != undefined ? ed.classList.add(props.addClass) : null;
+                let md = document.getElementsByClassName('search-true')[i];
+                if (props.hide === false) { null; }
+                else { md.innerHTML.slice(0, props.searchData.length).includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none"; }
+                props.addClass != undefined && md.innerHTML.slice(0, props.searchData.length).includes(props.searchData) ? ed.classList.add(props.addClass) : null;
             }
+
         }
     }
     else if (props.normalSearch) {
-        if (props.hideElement != undefined) {
+        if (props.hideAnotherElement != undefined) {
+            props.onSearch != undefined ? setTimeout(props.onSearch, 1) : null;
             for (let i = 0; i <= document.getElementsByClassName('search-true').length; i++) {
-                let ed = document.getElementsByClassName(props.hideElement)[i];
-                ed.innerHTML.includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none";
-                props.addClass != undefined ? ed.classList.add(props.addClass) : null;
+                let ed = document.getElementsByClassName(props.hideAnotherElement)[i];
+                let md = document.getElementsByClassName('search-true')[i];
+                if (props.hide === false) {
+                    null;
+                }
+                else {
+                    md.innerHTML.includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none";
+                } props.addClass != undefined && md.innerHTML.includes(props.searchData) ? ed.classList.add(props.addClass) : null;
             }
+
         }
         else {
+            props.onSearch != undefined ? setTimeout(props.onSearch, 1) : null;
             for (let i = 0; i <= document.getElementsByClassName('search-true').length; i++) {
                 let ed = document.getElementsByClassName('search-true')[i];
-                ed.innerHTML.includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none";
-                props.addClass != undefined ? ed.classList.add(props.addClass) : null;
+                let md = document.getElementsByClassName('search-true')[i];
+                if (props.hide === false) {
+                    null;
+                }
+                else {
+                    md.innerHTML.includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none";
+                } props.addClass != undefined && md.innerHTML.includes(props.searchData) ? ed.classList.add(props.addClass) : null;
             }
+
+        }
+    }
+    else {
+        props.onSearch != undefined ? setTimeout(props.onSearch, 1) : null;
+        for (let i = 0; i <= document.getElementsByClassName('search-true').length; i++) {
+            let ed = document.getElementsByClassName(props.hideAnotherElement)[i];
+            let md = document.getElementsByClassName('search-true')[i]
+            if (props.hide === false) {
+                null;
+            }
+            else { md.innerHTML.includes(props.searchData) ? ed.style.display = "block" : ed.style.display = "none"; } props.addClass != undefined && md.innerHTML.includes(props.searchData) ? ed.classList.add(props.addClass) : null;
         }
     }
 }
